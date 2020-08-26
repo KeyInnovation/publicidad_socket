5f5593270945411c69ebfee18c1dc7ff737a932f
Omar Montoya <luis.montoya.j@gmail.com>
2020-08-25 18:22:55 -0600

    Cambio de puerto

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const io = require("socket.io");
-const server = io.listen(3000);
+const server = io.listen(4568);
 
-server.on("connection", function (socket) {
-    
+server.on("connection", function(socket) {
+
     console.log("CONECTADO");
-    socket.on('refrescar', ()=>{
-        socket.broadcast.emit('refrescar');
+    socket.on('refrescar', (data) => {
+        console.log("REFRESCAR");
+        socket.broadcast.emit('refrescar', data);
     });
 
     socket.on("disconnect", () => {
